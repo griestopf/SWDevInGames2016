3cdb61204625b6b5544484a417d90717bdeab563
Christoph Müller <chrism at g-graphix.de>
2016-05-02 18:32:49 +0200

    Visitor Pattern implemented

---
+++ VisitorPattern/Core/SceneItem.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using Fusee.Engine.Common;
+using Fusee.Engine.Core;
+using Fusee.Math.Core;
+using Fusee.Xene;
+
+namespace Fusee.Tutorial.Core
+{
+
+    public class SceneItem
+    {
+        public virtual void Accept(Visitor visitor)
+        {
+        }
+    }
+
+    public class Material : SceneItem
+    {
+        public float3 Albedo;
+        public IShaderParam AlbedoParam;
+
+        public override void Accept(Visitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class Transform : SceneItem
+    {
+        public float3 Pos = float3.Zero;
+        public float3 Rot = float3.Zero;
+        public float3 Scale = float3.One;
+
+        public override void Accept(Visitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class Group : SceneItem
+    {
+        public List<SceneItem> Children;
+
+        public override void Accept(Visitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class Geometry : SceneItem
+    {
+        public Mesh Mesh;
+
+        public override void Accept(Visitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+
+    public abstract class Visitor 
+    {
+        public void TraverseChildren(Group group)
+        {
+            foreach (var child in group.Children)
+                child.Accept(this);
+        }
+
+        public virtual void Visit(Group group)
+        {
+
+        }
+
+        public virtual void Visit(Material mat)
+        {
+
+        }
+
+        public virtual void Visit(Transform xform)
+        {
+
+        }
+
+        public virtual void Visit(Geometry geometry)
+        {
+
+        }
+
+    }
+
+    public class RenderVisitor : Visitor
+    {
+        public RenderContext rc;
+        public override void Visit(Group group)
+        {
+            float4x4 mvOld = rc.ModelView;
+            TraverseChildren(group);
+            rc.ModelView = mvOld;
+        }
+
+        public override void Visit(Material mat)
+        {
+            rc.SetShaderParam(mat.AlbedoParam, mat.Albedo);
+        }
+
+        public override void Visit(Transform xform)
+        {
+            rc.ModelView = rc.ModelView *
+                                      float4x4.CreateTranslation(xform.Pos)
+                                      * float4x4.CreateRotationY(xform.Rot.y)
+                                      * float4x4.CreateRotationX(xform.Rot.x)
+                                      * float4x4.CreateRotationZ(xform.Rot.z)
+                                      * float4x4.CreateScale(xform.Scale);
+        }
+
+        public override void Visit(Geometry geometry)
+        {
+            rc.Render(geometry.Mesh);
+        }
+    }
+}
---
+++ VisitorPattern/Core/Tutorial.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Fusee.Base.Common;
+using Fusee.Base.Core;
+using Fusee.Engine.Common;
+using Fusee.Engine.Core;
+using Fusee.Math.Core;
+using Fusee.Serialization;
+using Fusee.Xene;
+using static Fusee.Engine.Core.Input;
+
+namespace Fusee.Tutorial.Core
+{
+
+    [FuseeApplication(Name = "Tutorial Example", Description = "The official FUSEE Tutorial.")]
+    public class Tutorial : RenderCanvas
+    {
+        private Mesh _mesh;
+        private const string _vertexShader = @"
+            attribute vec3 fuVertex;
+            attribute vec3 fuNormal;
+            uniform mat4 FUSEE_MVP;
+            uniform mat4 FUSEE_ITMV;
+            varying vec3 modelpos;
+            varying vec3 normal;
+            void main()
+            {
+                modelpos = fuVertex;
+                normal = normalize(mat3(FUSEE_ITMV) * fuNormal);
+                gl_Position = FUSEE_MVP * vec4(fuVertex, 1.0);
+            }";
+
+        private const string _pixelShader = @"
+            #ifdef GL_ES
+                precision highp float;
+            #endif
+            varying vec3 modelpos;
+            varying vec3 normal;
+            uniform vec3 albedo;
+
+            void main()
+            {
+                float intensity = dot(normal, vec3(0, 0, -1));
+                gl_FragColor = vec4(intensity * albedo, 1);
+            }";
+
+
+        private IShaderParam _albedoParam;
+        private float _alpha = 0.001f;
+        private float _beta;
+
+        private SceneItem _root;
+
+
+        public static Mesh LoadMesh(string assetName)
+        {
+            SceneContainer sc = AssetStorage.Get<SceneContainer>(assetName);
+            MeshComponent mc = sc.Children.FindComponents<MeshComponent>(c => true).First();
+            return new Mesh
+            {
+                Vertices = mc.Vertices,
+                Normals = mc.Normals,
+                Triangles = mc.Triangles
+            };
+        }
+
+        // Init is called on startup. 
+        public override void Init()
+        {
+            // Load some meshes
+            Mesh cone = LoadMesh("Cone.fus");
+            Mesh cube = LoadMesh("Cube.fus");
+            Mesh cylinder = LoadMesh("Cylinder.fus");
+            Mesh pyramid = LoadMesh("Pyramid.fus");
+            Mesh sphere = LoadMesh("Sphere.fus");
+
+
+            // Initialize the shader(s)
+            var shader = RC.CreateShader(_vertexShader, _pixelShader);
+            RC.SetShader(shader);
+            _albedoParam = RC.GetShaderParam(shader, "albedo");
+
+ 
+            // Setup a list of objects
+            _root = new Group()
+            {
+                Children = new List<SceneItem>(new SceneItem[]
+                {
+                    new Material { Albedo = new float3(0.2f, 0.9f, 0.1f), AlbedoParam = _albedoParam},
+                    // Body
+                    new Group
+                    {
+                        Children = new List<SceneItem>(new SceneItem[]
+                        {
+                            new Transform {Pos = new float3(0, 2.75f, 0), Scale = new float3(0.5f, 1, 0.25f)},
+                            new Geometry {Mesh = cube}
+                        })
+                    },
+
+                    // Legs
+                    new Group
+                    {
+                        Children = new List<SceneItem>(new SceneItem[]
+                        {
+                            new Transform {Pos = new float3(-0.25f, 1, 0), Scale = new float3(0.15f, 1, 0.15f)},
+                            new Geometry {Mesh = cylinder}
+                        })
+                    },
+
+                    new Group
+                    {
+                        Children = new List<SceneItem>(new SceneItem[]
+                        {
+                            new Transform {Pos = new float3(0.25f, 1, 0), Scale = new float3(0.15f, 1, 0.15f),},
+                            new Geometry {Mesh = cylinder}
+                        })
+                    },
+                })
+            };
+                
+            // Set the clear color for the backbuffer
+            RC.ClearColor = new float4(1, 1, 1, 1);
+        }
+
+        static float4x4 ModelXForm(float3 pos, float3 rot, float3 pivot)
+        {
+            return float4x4.CreateTranslation(pos + pivot)
+                   *float4x4.CreateRotationY(rot.y)
+                   *float4x4.CreateRotationX(rot.x)
+                   *float4x4.CreateRotationZ(rot.z)
+                   *float4x4.CreateTranslation(-pivot);
+        }
+
+        // RenderAFrame is called once a frame
+        public override void RenderAFrame()
+        {
+            // Clear the backbuffer
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+
+            float2 speed = Mouse.Velocity + Touch.GetVelocity(TouchPoints.Touchpoint_0);
+            if (Mouse.LeftButton || Touch.GetTouchActive(TouchPoints.Touchpoint_0))
+            {
+                _alpha -= speed.x*0.0001f;
+                _beta  -= speed.y*0.0001f;
+            }
+
+            // Setup matrices
+            var aspectRatio = Width / (float)Height;
+            RC.Projection = float4x4.CreatePerspectiveFieldOfView(3.141592f * 0.25f, aspectRatio, 0.01f, 20);
+            float4x4 view = float4x4.CreateTranslation(0, 0, 8)*float4x4.CreateRotationY(_alpha)*float4x4.CreateRotationX(_beta)*float4x4.CreateTranslation(0, -2f, 0);
+
+            RC.ModelView = view;
+
+            // TODO: Render the Root!!
+
+            
+            // Swap buffers: Show the contents of the backbuffer (containing the currently rendered frame) on the front buffer.
+            Present();
+        }
+
+
+        // Is called when the window was resized
+        public override void Resize()
+        {
+            // Set the new rendering area to the entire new windows size
+            RC.Viewport(0, 0, Width, Height);
+
+            // Create a new projection matrix generating undistorted images on the new aspect ratio.
+            var aspectRatio = Width/(float) Height;
+
+            // 0.25*PI Rad -> 45° Opening angle along the vertical direction. Horizontal opening angle is calculated based on the aspect ratio
+            // Front clipping happens at 1 (Objects nearer than 1 world unit get clipped)
+            // Back clipping happens at 2000 (Anything further away from the camera than 2000 world units gets clipped, polygons will be cut)
+            var projection = float4x4.CreatePerspectiveFieldOfView(3.141592f * 0.25f, aspectRatio, 1, 20000);
+            RC.Projection = projection;
+        }
+
+    }
+}
